05e1d9d7afc2c8c5b305e4806b66bda10460f5cc
Esdras Abreu <xstrikeesdraspb@gmail.com>
2022-10-24 16:40:51 -0300

    mudança de raiz atv4

--- Entities/Cliente.py
+++
@@ -1,4 +0,0 @@
-class Cliente:
-    def __init__(self, id: int, nome: str) -> None:
-        self.id = id
-        self.nome = nome
--- Repositories/PedidoRepository.py
+++
@@ -1,9 +0,0 @@
-from Entities.Pedido import Pedido
-
-
-class PedidoRepository:
-    def __init__(self) -> None:
-        self.lista_pedidos: list[Pedido] = []
-
-    def add_order(self, pedido: Pedido) -> None:
-        self.lista_pedidos.append(pedido)
